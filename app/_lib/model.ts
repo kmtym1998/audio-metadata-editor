452b58005e36db4758b7197b0829591cb86af64d
kmtym1998 <kmtym.devloper@gmail.com>
2023-07-23 13:12:33 +0900

    item 取得レスポンス model 定義

--- app/_lib/model.ts
+++ app/_lib/model.ts
@@ -3,3 +3,38 @@ export type DirEntry = {
   fullPath: string;
   isDir: boolean;
 };
+
+export type GetItemResult = {
+  audio: AudioMetadata;
+  fullPath: string;
+  isDir: boolean;
+  modTime: string;
+  mode: string;
+  name: string;
+  size: number;
+};
+
+export type AudioMetadata = {
+  album: string;
+  artist: string;
+  albumArtist: string;
+  comment: string;
+  composer: string;
+  disc: number[];
+  fileType: string;
+  format: string;
+  genre: string;
+  lyrics: string;
+  picture: Picture;
+  title: string;
+  track: number[];
+  year: number;
+};
+
+export type Picture = {
+  base64Img: string;
+  description: string;
+  ext: string;
+  mimeType: string;
+  type: string;
+};
